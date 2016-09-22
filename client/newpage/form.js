3119e4047fa373f083e51c8628ab3093c7639bec
zhangxinSourceTree <953698506@qq.com>
2016-09-22 17:32:47 +0800

    修改有误的地方

--- client/newpage/form.js
+++ client/newpage/form.js
@@ -4,19 +4,30 @@
 Template.formsub.events({
     "submit form":function (e) {
         e.preventDefault();
-
+        var name=$(e.target).find('[name=names]').val()
+        var age=$(e.target).find('[name=age]').val()
+        var tel=$(e.target).find('[name=tel]').val()
+        var birth=$(e.target).find('[name=birth]').val()
         var info=
         {
-            name:$(e.target).find('[name=names]').val(),
-            age:$(e.target).find('[name=age]').val(),
-            tel:$(e.target).find('[name=tel]').val(),
-            birth:$(e.target).find('[name=birth]').val(),
+            name:name,
+            age:age,
+            tel:tel,
+            birth:birth,
         };
-       Data.insert(info);
-        $(e.target).find('[name=names]').val("");
-        $(e.target).find('[name=age]').val("");
-        $(e.target).find('[name=tel]').val("");
-        $(e.target).find('[name=birth]').val("");
+        if(name==""||age==""||tel==""||birth=="")
+        {
+            alert("YOU Have Errors,Please Check!");
+        }
+        else
+            {
+                Data.insert(info);
+                $(e.target).find('[name=names]').val("");
+                $(e.target).find('[name=age]').val("");
+                $(e.target).find('[name=tel]').val("");
+                $(e.target).find('[name=birth]').val("");
+            }
+
     }
 
 });
